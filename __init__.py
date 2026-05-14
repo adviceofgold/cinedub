82f90e578ec81238a926f58a9d678684d660dea9
adviceofgold <93557721+adviceofgold@users.noreply.github.com>
2026-05-14 18:41:13 +0100

    Delete __init__.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from .core import Config, RuntimeMode, PipelineContext, Stage, StageResult, ModelManager
-from .checkpoint import CheckpointManager
-from .logging import CineDubLogger
-
-__version__ = "1.0.0"
